33274b75312b03b2f134df80d8582462c176a447
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-06 22:35:27 +0200

    1 - negative modulo are negative of modulo of negatiev

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    number_mod = -number
-number_mod = number % 10
+    number_mod = -((-number) % 10)
+else:
+    number_mod = number % 10
 print(f"Last digit of {number} is {number_mod} and is " +
       ("greater than 5" if (number_mod) > 5 else
        ("0" if (number_mod) == 0 else "less than 6 and not 0")))
