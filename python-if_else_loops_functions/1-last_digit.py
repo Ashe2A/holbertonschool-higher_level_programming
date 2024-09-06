38fec5faebdb55d95eb3db751635a15532c6f909
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-06 22:32:49 +0200

    1 - negative modulos

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {number % 10} and is " +
-      ("greater than 5" if (number % 10) > 5 else
-       ("0" if (number % 10) == 0 else "less than 6 and not 0")))
+if (number < 0):
+    number_mod = -number
+number_mod = number % 10
+print(f"Last digit of {number} is {number_mod} and is " +
+      ("greater than 5" if (number_mod) > 5 else
+       ("0" if (number_mod) == 0 else "less than 6 and not 0")))
