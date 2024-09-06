9cead616cd15939ea34f7b6bc20d114e7f2cea30
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-07 00:11:52 +0200

    8 - revert

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,8 +6,9 @@ def uppercase(str):
     Parameters:
         str (str): String to upper.
     """
-    for i in range(len(str) + 1):
+    for i in range(len(str)):
         if ord('a') <= ord(str[i]) <= ord('z'):
             print(chr(ord(str[i]) + ord('A') - ord('a')), end="")
         else:
             print(str[i], end="")
+    print("")
