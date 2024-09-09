4acdf9c683a4d8e8ba6be18a9336cd051a8086c1
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-09 13:31:12 +0200

    8 - no spaces

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def uppercase(str):
-	"""
-	Prints an uppercase string.
+    """
+    Prints an uppercase string.
 
-	Parameters:
-		str (str): String to upper.
+    Parameters:
+        str (str): String to upper.
 
-	Returns:
-		Nothing
-	"""
-	for i in range(len(str)):
-		print("{}".format(chr(ord(str[i]) + ord('A') - ord('a')))
-			  if ord('a') <= ord(str[i]) <= ord('z') else "{}".format(str[i]), end="")
-	print()
+    Returns:
+        Nothing
+    """
+    for i in range(len(str)):
+        print("{}".format(chr(ord(str[i]) + ord('A') - ord('a')))
+              if ord('a') <= ord(str[i]) <= ord('z') else "{}".format(str[i]), end="")
+    print()
