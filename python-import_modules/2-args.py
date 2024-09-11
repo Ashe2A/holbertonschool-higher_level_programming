f807e826e658e1fe4aab26eef61671e4f0161b7a
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-11 16:49:27 +0200

    2 - args in python

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    print("{} argument".format(len(argv)), end="")
+    if len(argv) == 0:
+        print("s.")
+    elif len(argv) == 1:
+        print(":")
+    else:
+        print("s:")
+    for i in range(1, len(argv)):
+        print("{}: {}".format(len(argv), argv[i - 1]))
