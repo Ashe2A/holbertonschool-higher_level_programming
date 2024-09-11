d5841e1d8bdae55688cc06a5feae5efef501ac4d
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-11 11:21:20 +0200

    11 - power

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Prints the b-power of a.
+
+    Parameters:
+        a (int): integer to power up.
+        b (int): power.
+
+    Returns:
+        a power b.
+    """
+    return (a ** b)
