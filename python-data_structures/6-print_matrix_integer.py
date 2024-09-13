f16b18dadd0a51150947f8c37b7b133cee73f733
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 14:59:10 +0200

    6 - removed space from ealrier commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -10,7 +10,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if j != i[len(i) - 1]:
-                print("{:d}".format(j), end="")
+                print("{:d}".format(j), end=" ")
             else:
                 print("{:d}".format(j))
 
