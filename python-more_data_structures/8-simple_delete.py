d1349a34fd124628f78f3310adf7e11544477ae1
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 20:55:45 +0200

    8 - del only if exists

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -10,5 +10,6 @@ def simple_delete(a_dictionary, key=""):
     Returns:
         Updated dictionary
     """
-    del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
