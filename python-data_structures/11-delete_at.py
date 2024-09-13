69bfe04b8fb97a8cf00752d30690aaba3c552bec
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 16:38:43 +0200

    11 - delete at index

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """
+    Remove list elements.
+
+    Parameters:
+        my_list (list) : item list.
+        idx : index of the item to remove
+
+    Returns:
+        modified (or not) list
+    """
+    if (my_list != []) or (idx < 0) or (idx > len(my_list)):
+        del(my_list[idx])
+    return my_list
