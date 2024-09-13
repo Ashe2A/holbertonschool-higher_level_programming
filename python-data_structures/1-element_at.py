d2e30e6d7890dd4daea36bbf873ad0635aae7541
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 13:23:49 +0200

    1 - element at

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+
+    Parameters:
+        my_list (list) : the list.
+        idx (int) : the element's index in the list
+
+    Returns:
+        The element in the lest, "None" if error
+    """
+    if (idx >= len(my_list)) or (idx < 0):
+        return "None"
+    return my_list[idx]
