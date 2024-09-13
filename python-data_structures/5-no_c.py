48b9493baa85a8f6d47b256b196aeaea251769e5
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 14:11:45 +0200

    5 - no c

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """
+    Removes c's from a string.
+
+    Parameters:
+        my_string (string) : the string.
+
+    Returns:
+        The modified (or not) string.
+    """
+    for i in my_string:
+        if (my_string[i - 1] == 'C') or (my_string[i - 1] == 'c'):
+            del(my_string[i - 1])
+    return my_string
