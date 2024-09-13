dc9e09590cac27ed75f06d6641456cef9a97a287
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 21:54:41 +0200

    9 - unneeded list

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -10,6 +10,6 @@ def multiply_by_2(a_dictionary):
         New updated dictionary
     """
     new_dictionary = []
-    for i in list(a_dictionary):
+    for i in a_dictionary:
         new_dictionary[i] = a_dictionary[i] * 2
     return new_dictionary
