5d201fbd14ba0bc1221e232f51ec8876452aa809
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 16:24:35 +0200

    9 - super max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """
+    Max integer.
+
+    Parameters:
+        my_list (list) : integer list.
+
+    Returns:
+        The max of an integer list
+    """
+    if my_list == []:
+        return "None"
+    super_max = my_list[0]
+    for i in my_list:
+        if i > super_max:
+            super_max = i
+    return super_max
