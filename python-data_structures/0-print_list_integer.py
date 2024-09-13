d90cb20ce36e79ddf63e756e3b5ccdd403b84cb6
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 13:13:55 +0200

    0 - decimal print

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """
-    Prints a list.
+    Prints a list of integers SPECIFICALLY.
 
     Parameters:
         my_list (list) : the list.
     """
     for i in my_list:
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
