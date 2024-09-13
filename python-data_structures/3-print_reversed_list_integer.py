fae6e795d58e73dfbf64a27ecde77c952c930b43
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 14:01:53 +0200

    3 - if it needed - 1, it doesn't need it in reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -8,4 +8,4 @@ def print_reversed_list_integer(my_list=[]):
         my_list (list) : the list.
     """
     for i in my_list:
-        print("{:d}".format(my_list[len(my_list) - 1 - i]))
+        print("{:d}".format(my_list[len(my_list) - i]))
