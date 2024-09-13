16495dc10a6d2c1d981785b0f7307ff81a28f95c
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 11:15:19 +0200

    0 - no range

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(1, len(my_list) + 1):
-        print("{}".format(my_list[i - 1]))
+    for i in my_list:
+        print("{}".format(my_list[i]))
