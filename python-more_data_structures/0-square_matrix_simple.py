08eaf29bd37c0d442754ee9c6c00fa5054e1975f
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 17:28:22 +0200

    0 - i[j] not new matrix[j]

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix[:]
     for i in new_matrix:
         for j in i:
-            new_matrix[j] = j * j
+            i[j] = j * j
     return new_matrix
