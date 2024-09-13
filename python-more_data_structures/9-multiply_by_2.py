c3dbfa7d4a55823243ff2ebc8beb5fcb78ab5569
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 22:07:35 +0200

    9 - initialize a list

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -9,7 +9,7 @@ def multiply_by_2(a_dictionary):
     Returns:
         New updated dictionary
     """
-    new_dictionary = []
-    for i in a_dictionary:
+    new_dictionary = {}
+    for i in list(a_dictionary):
         new_dictionary[i] = a_dictionary[i] * 2
     return new_dictionary
