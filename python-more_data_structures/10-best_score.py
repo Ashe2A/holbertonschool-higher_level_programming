48b8077ba0f86cc3202616880dd5056cc11f8692
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 23:54:36 +0200

    10 - best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    Best score
+
+    Parameters:
+        a_dictionary : a dictionary
+
+    Returns:
+        Highest value key in dictionary
+    """
+    best_score = list(a_dictionary)[0]
+    for i in list(a_dictionary):
+        if a_dictionary[i] > a_dictionary[best_score]:
+            best_score = i
+    return best_score
