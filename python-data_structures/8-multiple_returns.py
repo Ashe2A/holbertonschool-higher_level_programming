5ed7f2b00b153f832da8a509510045fbdde8c276
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 16:20:50 +0200

    8 - pycode (space)

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -11,5 +11,5 @@ def multiple_returns(sentence):
         Tuple (int, int/string) : the length and the first character
     """
     if (len(sentence) == 0):
-        return(0, "None")
+        return (0, "None")
     return (len(sentence), sentence[0])
