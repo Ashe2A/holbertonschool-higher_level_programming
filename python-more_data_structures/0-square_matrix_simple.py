21f97c693f37091f084d75596fc0ef47400741df
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 17:26:31 +0200

    0 - implement new matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix[:]
     for i in new_matrix:
         for j in i:
-            j *= j
+            new_matrix[j] = j * j
     return new_matrix
