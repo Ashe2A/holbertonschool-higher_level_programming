7ce8ffcf97c46bfd526023343348e91fd2517446
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 16:31:19 +0200

    10 - even true false

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    """
+    My list is divisible by 2.
+
+    Parameters:
+        my_list (list) : integer list.
+
+    Returns:
+        Boolean list of whetever the corresponding index in the list is divisible by 2.
+    """
+    boolist = []
+    for i in my_list:
+        if (i % 2) == 0:
+            boolist.append(True)
+        else:
+            boolist.append(False)
+    return boolist
