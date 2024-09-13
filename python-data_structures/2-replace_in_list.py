c327d0738dc2362dab5571679d56bc80d4635fe2
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 13:29:06 +0200

    2 - replace

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx, element):
+    """
+    Replaces an element in a list.
+
+    Parameters:
+        my_list (list) : the list.
+        idx (int) : the element's index in the list
+        element (void) : the element to insert
+
+    Returns:
+        The modified (or not) list
+    """
+    if 0 < idx < len(my_list):
+        my_list[idx] = element
+    return my_list[idx]
