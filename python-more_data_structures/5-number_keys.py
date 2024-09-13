ad7cc1cb55138038223380e767754ff497d39cb7
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 20:28:13 +0200

    5 - keys (I RTFM)

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Count keys from a dictionary.
+
+    Parameters:
+        a_dictionary : the dictionary.
+
+    Returns :
+        Length of key list from the dictionary.
+    """
+    return len(list(a_dictionary))
