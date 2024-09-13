13c3cbf2430697307903a61261dda70154f27a54
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 13:09:46 +0200

    0 - doc (output still bad)

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """
+    Prints a list.
+
+    Parameters:
+        my_list (list) : the list.
+    """
     for i in my_list:
         print("{}".format(my_list[i]))
