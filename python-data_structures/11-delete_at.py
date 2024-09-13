7103d138c7ee7dee7f8018c520a46e7d8bf6baa3
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-13 17:07:10 +0200

    11 - and for positive, or for negative

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -11,6 +11,6 @@ def delete_at(my_list=[], idx=0):
     Returns:
         modified (or not) list
     """
-    if (my_list != []) or (0 <= idx < len(my_list)):
+    if (my_list != []) and (0 <= idx < len(my_list)):
         del (my_list[idx])
     return my_list
