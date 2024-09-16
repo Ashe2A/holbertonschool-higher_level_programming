136148e023d5cee7ac8cb972ee020a3e81bd1b4d
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:12:09 +0200

    1 - new size is private

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -11,9 +11,9 @@ class Square:
     Square class with a size
     """
 
-    __size = 0
+    size = 0
 
-    def __init__(self, new_size):
+    def __init__(self, __new_size):
         self.is_new = True
-        if new_size is not None:
-            self.size = new_size
+        if __new_size is not None:
+            self.size = __new_size
