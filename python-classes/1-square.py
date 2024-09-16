ca7d11375b8e94c9987b558e25fed91c5677765c
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:24:46 +0200

    1 - syntax and requirments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -13,7 +13,6 @@ class Square:
 
     _size = 0
 
-    def __init__(self, new_size):
-        self.is_new = True
-        if new_size is not None:
-            self._size = new_size
+    def __init__(self, _Square__size):
+        if _Square__size is not None:
+            self._size = _Square__size
