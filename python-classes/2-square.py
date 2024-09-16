2a225f5cb39a5689f62284d5a286efe54e3c5801
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:51:42 +0200

    2 - reversed

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -13,10 +13,10 @@ class Square:
 
     def __init__(self, size=0):
         try:
+            self.__size = size
             if size < 0:
                 raise ValueError("size must be >= 0")
             if size is not int:
                 raise TypeError("size must be an integer")
-            self.__size = size
         except (TypeError, ValueError) as e:
             print(e)
