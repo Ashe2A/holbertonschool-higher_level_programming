ad9b6fb32b2ce4bc08817aec98c2ccafb36fdb83
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 15:52:11 +0200

    peer learning effective

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,7 +12,8 @@ class Square:
     """
 
     def __init__(self, size=0):
-        try:
-            self.__size = size
-        except (TypeError, ValueError) as e:
-            print(e)
+        if size < 0:
+            raise TypeError("size must be an integer")
+        if not (size is int):
+            raise ValueError("size must be >= 0")
+        self.__size = size
