5a1823cf48c23b8ede3d9105f2b072dfac112ede
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:35:42 +0200

    2 - square

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A class that defines any square
+"""
+
+
+class Square:
+
+    """
+    Square class with a size
+    """
+
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+        except ValueError:
+            if size < 0:
+                print("size must be >= 0")
+        except TypeError as e:
+            print("size must be an integer")
