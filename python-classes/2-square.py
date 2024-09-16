bca7ecc33960c2282b0578b6bf770dd52acd3350
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:51:10 +0200

    2 - learnt how to except in python

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -13,9 +13,10 @@ class Square:
 
     def __init__(self, size=0):
         try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            if size is not int:
+                raise TypeError("size must be an integer")
             self.__size = size
-        except ValueError:
-            if self.__size < 0:
-                print("size must be >= 0")
-        except TypeError:
-            print("size must be an integer")
+        except (TypeError, ValueError) as e:
+            print(e)
