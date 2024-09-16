c37d57db96f29d0008df6ff115d7114420b75085
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 17:54:27 +0200

    5 - new line if size = 0

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -36,6 +36,8 @@ class Square:
         self.__size = size
 
     def my_print(self):
+        if self.__size == 0:
+            print()
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
