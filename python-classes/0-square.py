6699f7e2598158eec06b12f6206793bee7558da3
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 11:28:09 +0200

    0 - square

---
+++ python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines any square"""
+    size = 0
---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    res = 0
+    if not ((roman_string is None) or (roman_string is not str)):
+        for i in roman_string:
+            if (i == 'M'):
+                res += 1000
+                if (i - 1 == 'C'):
+                    res -= 100
+            if (i == 'D'):
+                res += 500
+                if (i - 1 == 'C'):
+                    res -= 100
+            if (i == 'C'):
+                res += 100
+                if (i - 1 == 'X'):
+                    res -= 10
+            if (i == 'L'):
+                res += 50
+                if (i - 1 == 'X'):
+                    res -= 10
+            if (i == 'X'):
+                res += 10
+                if (i - 1 == 'I'):
+                    res -= 1
+            if (i == 'V'):
+                res += 5
+                if (i - 1 == 'I'):
+                    res -= 1
+            if (i == 'I'):
+                res += 1
+        return res
+    return 0
+
