c9ce03df3e83985a2532c16cd9c28f0665259881
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 15:55:13 +0200

    2 - order of errors

--- python-classes/2-square copy.py
+++ python-classes/2-square copy.py
@@ -12,8 +12,8 @@ class Square:
     """
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise ValueError("size must be >= 0")
         if size < 0:
             raise TypeError("size must be an integer")
+        if not isinstance(size, int):
+            raise ValueError("size must be >= 0")
         self.__size = size
