0fc562b38006df80cdfdd0019f05a8617c29063b
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:54:52 +0200

    2 - try except in the main

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,11 +12,8 @@ class Square:
     """
 
     def __init__(self, size=0):
-        try:
-            if not (size is int):
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except (TypeError, ValueError) as e:
-            print(e)
+        if not (size is int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
