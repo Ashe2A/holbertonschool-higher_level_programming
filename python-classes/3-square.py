9e39c1770838609667fbd5cea3863a46847a751a
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 16:36:09 +0200

    area and setter

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,11 +9,15 @@ class Square:
 
     """
     Square class with a positive integer size
+    and area available
     """
 
     def __init__(self, size=0):
-        if size < 0:
-            raise TypeError("size must be an integer")
         if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
