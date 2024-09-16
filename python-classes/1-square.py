663e5e18dba73cd89a08dc4d6309b96b566863e5
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-16 14:28:53 +0200

    1 - instance attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -11,8 +11,5 @@ class Square:
     Square class with a size
     """
 
-    _size = 0
-
-    def __init__(self, _Square__size):
-        if _Square__size is not None:
-            self._size = _Square__size
+    def __init__(self, size):
+        self.__size = size
