8905844360cd9c0b26a3e40bc0859defacbb773d
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-18 10:10:44 +0200

    6 - Correcting position typeerror conditions

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,9 +19,9 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.size = size
-        if (not isinstance(position[0], int))\
-            or (not isinstance(position[0], int))\
-                or (position[0] < 0) or (position[1] < 0):
+        if (not isinstance(self.position[0], int))\
+            or (not isinstance(self.position[1], int))\
+                or (self.position[0] < 0) or (self.position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.position = position
 
@@ -54,10 +54,10 @@ class Square:
     def position(self):
         return self.__position
 
-    @size.setter
+    @position.setter
     def position(self, position):
         if (not isinstance(self.position[0], int))\
-            or (not isinstance(self.position[0], int))\
+            or (not isinstance(self.position[1], int))\
                 or (self.position[0] < 0) or (self.position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
