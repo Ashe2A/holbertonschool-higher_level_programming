4891f00e0a1913e4efb9a7f279ae8f4b931ff44b
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-20 09:44:14 +0200

    8 - equal rectangles + exceptions

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -80,6 +80,10 @@ class Rectangle:
         print("Bye rectangle...")
 
     def bigger_or_equal(rect_1, rect_2):
-        if Rectangle.area(rect_1) > Rectangle.area(rect_2):
+        if (not isinstance(rect_1, Rectangle)):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if (not isinstance(rect_2, Rectangle)):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
             return rect_1
         return rect_2
