ab72d463953b376191fbacb21adcd2c42072916b
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-20 03:00:48 +0200

    7 - class attribute points to instance attribute

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
-        self.print_symbol = Rectangle.print_symbol
+        Rectangle.print_symbol = self.print_symbol
 
     @property
     def width(self):
