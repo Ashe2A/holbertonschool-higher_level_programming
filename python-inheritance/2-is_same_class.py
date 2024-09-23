2b627b4128b51c55d9b9cec0416e29985b697d49
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-23 13:46:22 +0200

    pycode

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,16 +6,15 @@ Is same class.
 
 
 def is_same_class(obj, a_class):
+    """
+    Checks if the object is of a class.
 
-        """
-        Checks if the object is of a class.
+    Paremeters:
+        obj: The object
+        a_class: a class$
 
-        Paremeters:
-                obj: The object
-                a_class: a class$
-
-        Return:
-                True is obj is from a class, False otherwise
-        """
-
-        return type(obj) == a_class
+    Return:
+        True is obj is from a class, False otherwise
+    """
+        
+    return type(obj) == a_class
