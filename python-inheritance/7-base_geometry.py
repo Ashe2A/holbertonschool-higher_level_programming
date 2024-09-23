a6fb275d09eb17cfefb07fa8d430e43f86c2a663
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-23 15:43:43 +0200

    7 - base geo elif

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-
 """
 A class that defines any geometric object
 """
 
 
 class BaseGeometry:
-
     """
     Improved geometry class
     with an abstract area
     and an integer value validator
     """
-
     def area(self):
-        raise Exception("area() is not implemented")
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
         return value
