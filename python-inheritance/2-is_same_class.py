097705aa716bd7438b0f748928de3a55a1b2d3c8
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-23 11:22:22 +0200

    2 - doc

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
+"""
+Is same class.
+"""
+
 def is_same_class(obj, a_class):
+        
         """
         Checks if the object is of a class.
         """
+
         return isinstance(obj, a_class)
