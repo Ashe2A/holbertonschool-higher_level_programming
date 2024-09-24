83f5561cdaf6788168e0ec38273d4cd7a04b8cab
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-24 13:09:47 +0200

    Perimeter edge case (width/height = 0)

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -100,6 +100,8 @@ class Rectangle(Shape):
             Perimeter of the Rectangle
         """
 
+        if (self.__width == 0) or (self.__height == 0):
+            return 0
         return 2 * (self.__width + self.__height)
 
 
