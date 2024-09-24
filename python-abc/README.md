1c73a807b1c27506e98ddc730a5ac3ff7cfafeec
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-24 10:17:30 +0200

    ABC: Animals (Dog & Cat)

---
+++ python-abc/README.md
@@ -0,0 +1,1 @@
+Abstract Base Classes in Python
---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+Animal classes
+"""
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    Animal abstract class
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        Animal sound abstract method
+        """
+
+        raise Exception("sound() is not implemented")
+
+class Dog(Animal):
+    """
+    Dog class
+    """
+
+    def sound(self):
+        """
+        Dog's sound
+        """
+
+        return "Bark"
+    
+class Cat(Animal):
+    """
+    Cat class
+    """
+
+    def sound(self):
+        """
+        Cat's sound
+        """
+
+        return "Meow"
