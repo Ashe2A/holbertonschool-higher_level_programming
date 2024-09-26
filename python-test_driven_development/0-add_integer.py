c73241b482e5321d167199719b459c5b417ca412
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 14:20:53 +0200

    0 - cast AFTER ?

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,8 @@ def add_integer(a, b=98):
     """
     Add two integers.
     """
-    a = int(a)
-    b = int(b)
-    if not (isinstance(a, int)):
+    if not (isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int)):
+    if not (isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
