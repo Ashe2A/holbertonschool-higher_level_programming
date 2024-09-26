b6bf9588828c6a405fbf1431882f79013e12bf73
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 15:01:46 +0200

    12 - strings

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,31 +3,31 @@ def roman_to_int(roman_string):
     res = 0
     if (not (roman_string is None)) and isinstance(roman_string, str):
         for i in roman_string:
-            if (i is 'M'):
+            if (i is "M"):
                 res += 1000
-                if (i - 1 is 'C'):
+                if (i - 1 is "C"):
                     res -= 100
-            if (i is 'D'):
+            if (i is "D"):
                 res += 500
-                if (i - 1 is 'C'):
+                if (i - 1 is "C"):
                     res -= 100
-            if (i is 'C'):
+            if (i is "C"):
                 res += 100
-                if (i - 1 is 'X'):
+                if (i - 1 is "X"):
                     res -= 10
-            if (i is 'L'):
+            if (i is "L"):
                 res += 50
-                if (i - 1 is 'X'):
+                if (i - 1 is "X"):
                     res -= 10
-            if (i is 'X'):
+            if (i is "X"):
                 res += 10
-                if (i - 1 is 'I'):
+                if (i - 1 is "I"):
                     res -= 1
-            if (i is 'V'):
+            if (i is "V"):
                 res += 5
-                if (i - 1 is 'I'):
+                if (i - 1 is "I"):
                     res -= 1
-            if (i is 'I'):
+            if (i is "I"):
                 res += 1
         return res
     else:
