9d090a9ebb51074d9bc58640495a035ca2fbe56d
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 15:17:26 +0200

    12 - i can't be -1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,31 +3,31 @@ def roman_to_int(roman_string):
     res = 0
     if (not (roman_string is None)) and isinstance(roman_string, str):
         for i in range(len(roman_string)):
-            if (roman_string[i] == "M"):
+            if roman_string[i] == "M":
                 res += 1000
-                if (roman_string[i - 1] == "C"):
+                if (i > 0) and (roman_string[i - 1] == "C"):
                     res -= 200
-            elif (roman_string[i] == "D"):
+            elif roman_string[i] == "D":
                 res += 500
-                if (roman_string[i - 1] == "C"):
+                if (i > 0) and (roman_string[i - 1] == "C"):
                     res -= 200
-            elif (roman_string[i] == "C"):
+            elif roman_string[i] == "C":
                 res += 100
-                if (roman_string[i - 1] == "X"):
+                if (i > 0) and (roman_string[i - 1] == "X"):
                     res -= 20
-            elif (roman_string[i] == "L"):
+            elif roman_string[i] == "L":
                 res += 50
-                if (roman_string[i - 1] == "X"):
+                if (i > 0) and (roman_string[i - 1] == "X"):
                     res -= 20
-            elif (roman_string[i] == "X"):
+            elif roman_string[i] == "X":
                 res += 10
-                if (roman_string[i - 1] == "I"):
+                if (i > 0) and (roman_string[i - 1] == "I"):
                     res -= 2
-            elif (roman_string[i] == "V"):
+            elif roman_string[i] == "V":
                 res += 5
-                if (roman_string[i - 1] == "I"):
+                if (i > 0) and (roman_string[i - 1] == "I"):
                     res -= 2
-            elif (roman_string[i] == "I"):
+            elif roman_string[i] == "I":
                 res += 1
         return res
     else:
