7b18f907dc1b8249bb2d8cbed03c77b3225b81dc
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 11:20:53 +0200

    0 - except no newline

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             res += 1
         except:
-            print("")
+            print("", end="")
     print()
     return res
