4987220e27fa634fccf5c25b8e1bcae707a31217
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 11:31:04 +0200

    1 - value error not index

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -8,6 +8,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except IndexError:
+    except ValueError:
         print("{} is not an integer".format(value))
     return False
