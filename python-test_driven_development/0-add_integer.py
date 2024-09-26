612b3e14b2d55082a4ca9cd1685928b9af8ef599
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 14:18:27 +0200

    0 - if they are float is not mandatory

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,8 @@ def add_integer(a, b=98):
     """
     Add two integers.
     """
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
+    a = int(a)
+    b = int(b)
     if not (isinstance(a, int)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int)):
