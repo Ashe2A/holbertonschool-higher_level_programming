4d37b1c6305943b7faaddf4a572bb0238d12a9ca
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 11:26:31 +0200

    1 - safe print int

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     """
     Safe print list.
     """
+
     res = 0
     for i in range(x):
         try:
---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """
+    Safe print integer.
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except IndexError:
+        print("{} is not an integer".format(value))
+    return False
