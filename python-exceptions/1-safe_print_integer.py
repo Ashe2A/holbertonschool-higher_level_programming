22d53ab6e0391ded67264ff954ac1aed2e3a0331
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 11:34:28 +0200

    1 - both errors should work together

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -8,6 +8,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        pass
-    return False
+    except (TypeError, ValueError):
+        return False
