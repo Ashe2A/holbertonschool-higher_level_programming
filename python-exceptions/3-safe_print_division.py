d256766dc5b8f26b3a638ff55abae031ebbf092f
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 12:10:28 +0200

    3 - None managing

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,9 +6,9 @@ def safe_print_division(a, b):
     """
 
     try:
-        return a / b
+        res = a / b
     except (TypeError, ValueError, ZeroDivisionError):
-        pass
+        res = None
     finally:
-        print("Inside result: {}".format(a / b))   
-    return None
+        print("Inside result: {}".format(res))
+    return res
