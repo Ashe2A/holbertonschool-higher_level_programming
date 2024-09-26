e71437e07530180d9ed41f9d6bb358bd2e8d1817
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 13:03:46 +0200

    4 - list division

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Safe printing of integer division of two lists.
+    """
+
+    list_div = []
+    for i in range(my_list_1):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except (TypeError, ValueError):
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            list_div.append(div)
+    return list_div
