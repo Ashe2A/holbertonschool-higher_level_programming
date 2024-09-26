1854ed99cbe6cf5fb5036b7169310fcb5be1ef37
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 12:02:28 +0200

    3 - Integer division's safe printing

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    Integer division's safe printing.
+    """
+
+    try:
+        print("Inside result: {}".format(a / b))
+        return a / b
+    except (TypeError, ValueError):
+        pass
+    finally:
+        print("Inside result: None")
+    return None
