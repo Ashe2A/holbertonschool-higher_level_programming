75150cef8810cd245b33e71db0d806323eef74da
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-26 14:23:09 +0200

    0 - pycode??

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+""" Tests function to add integers with exceptions """
+
+
 def add_integer(a, b=98):
-    """
-    Add two integers.
-    """
+    """ Add two integers """
     if not (isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
     if not (isinstance(b, (int, float))):
