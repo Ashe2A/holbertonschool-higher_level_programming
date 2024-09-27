d5110200ce69278bea5d8a762db4eda883e2daba
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-27 11:12:39 +0200

    2 - divide matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Tests function to divide matrix integers with exceptions """
+
+
+def matrix_divided(matrix, div):
+    """ Divide matrix """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if not (isinstance(matrix[i][j], (int, float))):
+                raise TypeError("matrix must be a matrix (list of lists)\
+                                of integers/floats")
+
+    zerolen = len(matrix[0])
+    for i in matrix:
+        if len(i) != zerolen:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not (isinstance(div, (int, float))):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = (round(j / div, 2))
+
+    return new_matrix
