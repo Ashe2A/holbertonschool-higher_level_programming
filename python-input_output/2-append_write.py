5e47360789e54b6a3fba3368534ad2303b5e7d88
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 10:50:56 +0200

    2 - append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Append file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appending the file.
+    """
+
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        return file.write(text)
