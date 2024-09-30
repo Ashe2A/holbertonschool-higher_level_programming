22d920b528a100733e1696cc2aa12c9100471857
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 10:59:45 +0200

    3 - json dumps

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,7 +9,5 @@ def append_write(filename="", text=""):
     """
     Appending the file.
     """
-
-
     with open(filename, 'a', encoding="utf-8") as file:
         return file.write(text)
---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Returns JSON info of string.
+"""
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returning the JSON info of string.
+    """
+    return json.dumps(my_obj)
