730c558ffbb55cc1493cee0f6b6ab869af3388a1
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 16:31:02 +0200

    7 - load returns list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,6 @@ Loads object from JSON file.
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-append_write = __import__('2-append_write').append_write
-from_json_string = __import__('4-from_json_string').from_json_string
 import sys
 
 args = []
@@ -16,5 +14,6 @@ for i in sys.argv:
 args.pop(0)
 
 filename = "add_item.json"
-new_json = append_write(filename, args)
-save_to_json_file(load_from_json_file(filename), filename)
+append_json = load_from_json_file(filename)
+append_json.append(args)
+save_to_json_file(append_json, filename)
