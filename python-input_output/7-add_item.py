99b7dfc69f65e1d0aff4cf870c2267b3a73b4b60
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 16:14:53 +0200

    7 - nested functions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,6 +6,8 @@ Loads object from JSON file.
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+append_write = __import__('2-append_write').append_write
+from_json_string = __import__('4-from_json_string').from_json_string
 import sys
 
 args = []
@@ -14,5 +16,5 @@ for i in sys.argv:
 args.pop(0)
 
 filename = "add_item.json"
-save_to_json_file(args, filename)
+new_json = append_write(filename, args)
 save_to_json_file(load_from_json_file(filename), filename)
