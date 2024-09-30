fb06802515cb424c7a7218c31bcc575dc0156f1e
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 15:39:46 +0200

    12 - out of range?

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -17,8 +17,8 @@ def pascal_triangle(n):
             triangle.append([])
             for j in range(i):
                 if 0 < j < i:
-                    triangle[i].append(triangle[i - 1][j - 1]\
-                                       + triangle[i - 1][j])
+                    app = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                    triangle[i].append(app)
                 else:
                     triangle[i].append(1)
     return triangle
