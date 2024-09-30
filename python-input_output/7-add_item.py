21984826469a212e9625d32319919d330a0d3395
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 16:00:18 +0200

    7 - add items

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,5 +12,6 @@ args = []
 for i in sys.argv:
     args.append(i)
 
-with open("add_item.json", 'w', encoding="utf-8") as file:
-    file.write(str(args))
+
+save_to_json_file(args, "add_item.json")
+load_from_json_file("add_item.json")
