10b1712875134b6390ee844f943958fe55b3668f
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 13:55:55 +0200

    6 - load from read

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Loads object from JSON file.
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Loading object from JSON file.
+
+    Parameters:
+        my_obj: object.
+        filename (string): the file to convert.
+    
+    Return:
+        The object from the JSON file
+    """
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        json.load(file.read())
