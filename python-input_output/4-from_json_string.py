1aa16600552673d84a38b15b1d2d20e0f6125b73
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 11:09:38 +0200

    4 - return obj

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Returns object with a JSON info.
+"""
+
+import json
+
+
+def from_json_string(my_obj):
+    """
+    Returning the object with JSON info.
+    """
+
+    return my_obj
