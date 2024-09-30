b717817d208ed8ff9c9fb5c99c075b52dbfd3cc6
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-09-30 16:59:35 +0200

    7 - pycode

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-"""
-Loads object from JSON file.
-"""
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
+
+
+"""
+Loads object from JSON file.
+"""
 
 args = []
 for i in sys.argv:
