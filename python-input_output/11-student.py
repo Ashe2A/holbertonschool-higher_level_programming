787ad73705b8cc1f9f240be7251d6002ce103a0c
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-01 14:51:57 +0200

    11 - dict = json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Student.
+"""
+
+
+class Student:
+
+    """
+    Student class.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            dict_only_str = {}
+            for i in attrs:
+                if isinstance(i, str) and hasattr(self, i):
+                    dict_only_str[i] = getattr(self, i)
+            return dict_only_str
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__ = json
