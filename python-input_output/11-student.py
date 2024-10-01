c8fbad12c3e153edc021eccc68c4c39e4c210681
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-01 14:54:19 +0200

    11 - only if list

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -26,4 +26,5 @@ class Student:
             return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dict__ = json
+        if isinstance(json, list):
+            self.__dict__ = json
