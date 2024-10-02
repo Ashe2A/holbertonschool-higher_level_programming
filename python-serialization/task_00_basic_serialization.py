cd462c853260f470e8436253974dbf4fcf160e55
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-02 10:58:23 +0200

    00 - basic serializeation

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,22 +4,19 @@ Basic serialization.
 """
 
 import json
-import os
 
 
 def serialize_and_save_to_file(data, filename):
     """
     Serialization.
     """
-    if os.path.exists(filename):
-        os.remove(filename)
-    with open(filename, 'w') as file:
-        json.dumps(data, file)
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(json.dumps(data))
 
 
 def load_and_deserialize(filename):
     """
     Deserialization.
     """
-    with open(filename, 'r') as file:
-        return json.loads(file)
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.loads(file.read())
--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 """
-Basic serialization with pickle.
+Pickle serialization.
 """
 
 import pickle
-import os
-
 
 def serialize_and_save_to_file(data, filename):
     """
-    Serialization with pickle.
+    Pickle Serialization.
     """
-    if os.path.exists(filename):
-        os.remove(filename)
     with open(filename, 'wb') as file:
         pickle.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """
-    Deserialization with pickle.
+    Pickle Deserialization.
     """
     with open(filename, 'rb') as file:
         return pickle.load(file)
