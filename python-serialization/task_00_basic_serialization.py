a86796834b681604e6a76bc0414094e64f730e4b
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-02 10:25:33 +0200

    00 - pycode

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 
+"""
+Basic serialization.
+"""
+
 import pickle
 
-def serialize_and_save_to_file(data, filename):
+
+def serialize_and_save_to_file(data, filename)
+    """
+    Serialization.
+    """
+
     with open(filename, 'wb') as file:
         pickle.dump(data, file)
 
+
 def load_and_deserialize(filename):
+    """
+    Deserialization.
+    """
+
     with open(filename, 'rb') as file:
         loaded_file = pickle.load(file)
     return loaded_file
