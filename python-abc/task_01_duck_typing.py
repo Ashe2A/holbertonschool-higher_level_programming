bfdd25a323f3f118fa699f24e279ffc41997b767
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-14 10:13:33 +0200

    01 - public attributes + no exceptions

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -38,25 +38,7 @@ class Circle(Shape):
     pi = math.pi
 
     def __init__(self, radius=0):
-        if not isinstance(radius, int):
-            raise TypeError("Radius must be an integer")
-        elif radius < 0:
-            raise ValueError("Radius must be positive or zero")
-        else:
-            self.__radius = radius
-
-    @property
-    def radius(self):
-        return self.__radius
-    
-    @radius.setter
-    def radius(self, radius):
-        if not isinstance(radius, int):
-            raise TypeError("Radius must be an integer")
-        elif radius < 0:
-            raise ValueError("Radius must be positive or zero")
-        else:
-            self.__radius= radius
+        self.radius = radius
 
     def area(self):
         """
@@ -69,7 +51,7 @@ class Circle(Shape):
             Area of the Circle
         """
 
-        return Circle.pi * (self.__radius ** 2)
+        return Circle.pi * (self.radius ** 2)
 
     def perimeter(self):
         """
@@ -82,7 +64,7 @@ class Circle(Shape):
             Circumference of the Circle
         """
 
-        return 2 * Circle.pi * self.__radius
+        return 2 * Circle.pi * self.radius
 
 
 class Rectangle(Shape):
@@ -91,43 +73,8 @@ class Rectangle(Shape):
     """
 
     def __init__(self, width=0, height=0):
-        if (not isinstance(width, int)):
-            raise TypeError("Width must be integers")
-        elif (not isinstance(height, int)):
-            raise TypeError("Height must be integers")
-        elif (width < 0):
-            raise ValueError("Width must be positive or zero")
-        elif (height < 0):
-            raise ValueError("Height must be positive or zero")
-        else:
-            self.__width = width
-            self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
-    
-    @width.setter
-    def width(self, width):
-        if not isinstance(width, int):
-            raise TypeError("Width must be an integer")
-        elif width < 0:
-            raise ValueError("Width must be positive or zero")
-        else:
-            self.__width = width
-
-    @property
-    def height(self):
-        return self.__height
-    
-    @height.setter
-    def height(self, height):
-        if not isinstance(height, int):
-            raise TypeError("Height must be integers")
-        elif height < 0:
-            raise ValueError("Height must be positive or zero")
-        else:
-            self.__height = height
+        self.width = width
+        self.height = height
 
     def area(self):
         """
@@ -140,7 +87,7 @@ class Rectangle(Shape):
             Area of the Rectangle
         """
 
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
         """
@@ -153,9 +100,9 @@ class Rectangle(Shape):
             Perimeter of the Rectangle
         """
 
-        if (self.__width == 0) or (self.__height == 0):
+        if (self.width == 0) or (self.height == 0):
             return 0
-        return 2 * (self.__width + self.__height)
+        return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
