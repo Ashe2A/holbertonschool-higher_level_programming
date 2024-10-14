947fa10a49342c4e2fe71cece290f6f42ed9c5bf
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-14 16:45:33 +0200

    03 - counted iterator

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Counted Iterator
+"""
+
+import ABC
+
+
+class CountedIterator(ABC):
+    """
+    Updated talkative list
+    """
+
+    def __init__(self, some_iterable):
+        self.iterator = iter(some_iterable)
+        self.counter = 0
+    
+    @property
+    def get_count(self):
+        return self.counter
+
+    def __next__(self, iterable):
+        if self.counter >= len(self.iterator) - 1:
+            raise StopIteration()
+        else:
+            print(iterable[self.counter])
+            self.counter += 1
