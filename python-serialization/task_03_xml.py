62bfa9753dab00c7d349e9fffc796f1f0c93db42
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-10-14 18:01:28 +0200

    03 - tree + return + element xml attribute (tag & text)

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -7,15 +7,17 @@ import xml.etree.ElementTree as ET
 
 
 def serialize_to_xml(dictionary, filename):
-    json_to_xml = "<data>"
+    json_to_xml = "<data>\n"
     for i in dictionary:
-        json_to_xml += "<{}>{}</{}>".format(i, dictionary[i], i)
+        json_to_xml += "    <{}>{}</{}>\n".format(i, dictionary[i], i)
     json_to_xml += "</data>"
     with open(filename, 'w', encoding="utf-8") as file:
         return file.write(json_to_xml)
 
 def deserialize_from_xml(filename):
     xml_data = ET.parse(filename)
+    root = xml_data.getroot()
     xml_to_json = {}
-    for i in xml_data:
-        xml_to_json[i] = xml_data[i]
+    for element in root:
+        xml_to_json[element.tag] = element.text
+    return xml_to_json
