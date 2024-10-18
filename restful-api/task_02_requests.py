f811f961e36afd5d772f558f3643519e16112582
Ashe2A <supersonicx001@gmail.com>
2024-10-18 17:05:07 +0200

    fetch_and_print_posts

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+import requests
+
 def fetch_and_print_posts():
-    fetch_post = requests.get()
-    print("Status Code: {}".format(fetch_post["id"]))
+    fetch_post = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print("Status Code: {}".format(fetch_post.status_code))
+
+    if fetch_post.status_code == 200:
+        json_ed = fetch_post.json()
+
+    for i in json_ed:
+        print(i["title"])
