d0788aaa12b2d5203a02a0ce3d5ca1f9028b2dc6
Ashe2A <supersonicx001@gmail.com>
2024-10-20 18:44:50 +0200

    02 - requests save

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-import requests
+import requests, csv
+
 
 def fetch_and_print_posts():
     fetch_post = requests.get('https://jsonplaceholder.typicode.com/posts')
@@ -9,5 +10,32 @@ def fetch_and_print_posts():
     if fetch_post.status_code == 200:
         json_ed = fetch_post.json()
 
-    for i in json_ed:
-        print(i["title"])
+        for i in json_ed:
+            print(i["title"])
+
+    else:
+        print("Error {}".format(fetch_post.status_code))
+
+
+def fetch_and_save_posts():
+    fetch_post = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print("Status Code: {}".format(fetch_post.status_code))
+
+    if fetch_post.status_code == 200:
+        json_ed = fetch_post.json()
+
+        dict_list = []
+        for i in json_ed:
+            new_dict = {}
+            new_dict["id"] = i["id"]
+            new_dict["title"] = i["title"]
+            new_dict["body"] = i["body"]
+            dict_list.append(new_dict)
+
+        with open("posts.csv", 'w', encoding="utf-8") as file:
+            write_to_csv = csv.DictWriter(file, keys=["id", "title", "body"])
+            write_to_csv.writeheader()
+            write_to_csv.writerows(dict_list)
+
+    else:
+        print("Error {}".format(fetch_post.status_code))
