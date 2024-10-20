1d591031b2c9308b05eea8e6d2a7f268b7ef0972
Ashe2A <supersonicx001@gmail.com>
2024-10-20 18:56:20 +0200

    02 - no print for save

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,7 +19,6 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     fetch_post = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print("Status Code: {}".format(fetch_post.status_code))
 
     if fetch_post.status_code == 200:
         json_ed = fetch_post.json()
