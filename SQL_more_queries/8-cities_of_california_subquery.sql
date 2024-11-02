432c6f55a0591c9a722eb3227d693fc1e40a1e1c
Ashe2A <supersonicx001@gmail.com>
2024-11-03 00:16:25 +0100

    8 - no need to list state_id (subquery back)

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,9 @@
 -- Cities of California:
-SELECT c.*
-FROM states AS s, cities AS c
-WHERE c.state_id = s.id AND s.name = "California"
+SELECT c.id, c.name
+FROM (
+    SELECT *
+    FROM states AS s
+    WHERE s.name = "California"
+    ) AS s, cities AS c
+WHERE c.state_id = s.id
 ORDER BY c.id;
