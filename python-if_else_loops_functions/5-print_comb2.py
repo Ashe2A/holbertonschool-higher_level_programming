0848306c8703559f8b8e84e4392f84c99adc64e3
Ashe2A <supersonicx001@gmail.com>
2025-01-16 20:07:38 +0100

    IF/ELSE 5/6 - limiting prints ({:02} + use of end attribute)

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("0", end="")
-    print("{}".format(i), end="")
     if i < 99:
-        print(", ", end="")
+        print("{:02}".format(i), end=", ")
     else:
-        print()
+        print("{}".format(i))
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if (i / 10) < (i % 10):
-        if i < 10:
-            print("0", end="")
-        print("{}".format(i), end="")
+        print("{:02}".format(i), end="")
         if i < 89:
             print(", ", end="")
         else:
