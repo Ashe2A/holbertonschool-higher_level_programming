118cdfe0fcafe24b9c9028f876ea08b8cc832c68
Ashe2A <supersonicx001@gmail.com>
2025-01-20 23:53:43 +0100

    Data Structures - (len - 1) + task 1 (copy error)

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+def element_at(my_list, idx):
+    if 0 < idx < len(my_list) - 1:
+        return my_list[idx]
+    return None
---
+++ python-data_structures/README.md
@@ -0,0 +1,1 @@
+# [Python - Data Structures: Lists, Tuples](https://intranet.hbtn.io/projects/2120)
