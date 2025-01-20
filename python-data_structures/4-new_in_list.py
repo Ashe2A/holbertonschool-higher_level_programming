b388ac7372a7fd73ed3005a0e789687af0bc99c8
Ashe2A <supersonicx001@gmail.com>
2025-01-21 00:32:28 +0100

    Data Structures - 0 index counts

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for i in my_list:
         new_list.append(i)
-    if (0 < idx < len(new_list)) and (len(new_list) > 0):
+    if 0 <= idx < len(new_list):
         new_list[idx] = element
     return new_list
