5f632830bc2669d8a82e1312702a2bdd3744f692
Ashe2A <supersonicx001@gmail.com>
2025-01-26 21:18:09 +0100

    TDD - (0) not a number & infinity

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+Add integer.
+add_integer is the function to add integers
+Usage: x = add_integer(a, b)
+"""
+
 def add_integer(a, b=98):
+    """
+    Add two integers together.
+    
+    Params:
+        a: first integer
+        b: second
+
+    Return:
+        Sum of a and b
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
