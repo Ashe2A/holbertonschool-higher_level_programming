cd130b60fc5edb4e88617ef870e10119281af2df
Ashe2A <supersonicx001@gmail.com>
2025-01-27 19:42:24 +0100

    Exceptions - (102) print test calls removal

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -12,9 +11,3 @@ def magic_calculation(a, b):
             result += (a ** b) / i
         result = b + a
     return result
-
-print(magic_calculation(1, 2))
-print(magic_calculation(1, 4))
-print(magic_calculation(4, 1))
-print(magic_calculation(3, 3))
-print(magic_calculation(1, 1))
