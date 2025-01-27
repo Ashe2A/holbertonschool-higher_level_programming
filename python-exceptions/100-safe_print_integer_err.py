a467daa8dff9acec97dafc47469fdcaf111a881a
Ashe2A <supersonicx001@gmail.com>
2025-01-27 15:29:43 +0100

    Exceptions - (100) exception syntax

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exception : {}".format(e), file=stderr)
+        print("Exception: {}".format(e), file=stderr)
         return False
