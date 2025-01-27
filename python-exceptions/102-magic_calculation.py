7c16f70f4db2cea618c6ee4f39ce3a46868cb44d
Ashe2A <supersonicx001@gmail.com>
2025-01-27 19:41:05 +0100

    Exceptions - (102) binary add to result, not inplace add

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        if i <= a:
-            raise Exception('Too far')
+        try:
+            if i > a:
+                raise Exception('Too far')
+        except Exception:
+            pass
         else:
             result += (a ** b) / i
-    return result + b + a
+        result = b + a
+    return result
+
+print(magic_calculation(1, 2))
+print(magic_calculation(1, 4))
+print(magic_calculation(4, 1))
+print(magic_calculation(3, 3))
+print(magic_calculation(1, 1))
