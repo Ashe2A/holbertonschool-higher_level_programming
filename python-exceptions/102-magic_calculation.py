296d38b8ce9cbd76296953a1352db6dde2043e8f
Ashe2A <supersonicx001@gmail.com>
2025-01-27 18:26:43 +0100

    Exceptions - (102) no dis

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -9,10 +7,8 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except Exception:
-            pass
+        except Exception as e:
+            print(e)
         finally:
             result += b + a
     return result
-            
-dis.dis(magic_calculation)
