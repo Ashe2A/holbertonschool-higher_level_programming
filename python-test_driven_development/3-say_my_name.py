8cd9da378b17765d1533134a203e67654db5e943
Ashe2A <supersonicx001@gmail.com>
2025-01-27 10:30:14 +0100

    TDD - (3) say my name (init)

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Say my name.
+Usage: say_my_name(first_name, last_name)
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print a full name.
+
+    Params:
+        first_name: The first name
+        last_name: The last name
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    print("My name is {}".format(first_name), end=" ")
+    if not (isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+    print("{}".format(last_name))
