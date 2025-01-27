8114cbf5d86580707cbf22eaa91ac7ac7033faa2
Ashe2A <supersonicx001@gmail.com>
2025-01-27 17:55:30 +0100

    Exceptions - (102) exception in bytcode

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import dis
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        if i > a:
+            raise Exception('Too far')
+        else:
+            result += (a ** b) / i
+        result += b + a
+    return result
+            
+dis.dis(magic_calculation)
