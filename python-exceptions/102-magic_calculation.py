95a6f07b85607253de192fa5ee4932c34087ac0f
Ashe2A <supersonicx001@gmail.com>
2025-01-27 17:58:16 +0100

    Exceptions - (102) try except finally lines

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -4,11 +4,15 @@ import dis
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        if i > a:
-            raise Exception('Too far')
-        else:
-            result += (a ** b) / i
-        result += b + a
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except Exception as e:
+            print(e)
+        finally:
+            result += b + a
     return result
             
 dis.dis(magic_calculation)
