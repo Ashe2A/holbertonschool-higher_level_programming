34108bb8ba461f126c648966dd1a6e99df2d9e29
Ashe2A <supersonicx001@gmail.com>
2025-01-27 18:34:23 +0100

    Exceptions - (102) no try except

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,11 +2,8 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-        except Exception:
-            pass
+        if i > a:
+            raise Exception('Too far')
         else:
             result += (a ** b) / i
     return result + b + a
