350fce301e7a038676977eeb137ef8e3bdb09918
Ashe2A <supersonicx001@gmail.com>
2025-01-29 10:02:28 +0100

    OOP - (4) *property and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,20 +21,6 @@ class Square:
             else:
                 raise ValueError("size must be >= 0")
         else:
-            raise TypeError("size must be an integer")                         
-
-    @property.getter
-    def size(self):
-        return self.__size
-
-    @property.setter
-    def size(self, value):
-        if isinstance(value, int):
-            if value >= 0:
-                self.__size = value
-            else:
-                raise ValueError("size must be >= 0")
-        else:
             raise TypeError("size must be an integer")
 
     def area(self):
@@ -44,3 +30,17 @@ class Square:
             int: square of the size (area)
         """
         return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
