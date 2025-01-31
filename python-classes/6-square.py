bd345fc2d3cc3d4f7237dbb5b97cbcae19e19286
Ashe2A <supersonicx001@gmail.com>
2025-01-31 21:33:25 +0100

    OOP - (6) condition for true fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -90,8 +90,8 @@ class Square:
         if isinstance(value, tuple) and \
             len(value) == 2 and \
                 isinstance(value[0], int) and \
-                    isinstance(value[1], int) and \
-                        value[0] >= 0 or value[1] >= 0:
+                isinstance(value[1], int) and \
+                value[0] >= 0 or value[1] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
