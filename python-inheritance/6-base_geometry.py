dd89bf7215dc48ca73464274ce093ee82d54b2cb
Ashe2A <supersonicx001@gmail.com>
2025-02-07 12:57:04 +0100

    Inheritance - (6) geometry interface

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Base geometry module.
+"""
+
+
+class BaseGeometry():
+    """
+    Basic geometry class.
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
