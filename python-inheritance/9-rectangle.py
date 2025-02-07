905590ac6ea8de40a8dd8029e687da50dcc4f4f5
Ashe2A <supersonicx001@gmail.com>
2025-02-07 17:10:57 +0100

    Inheritance - (9) Area and string representation

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -30,3 +30,14 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Area of the Rectangle
+
+        Returns:
+            int: width times height
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}/{}".format(__class__.__name__, self.__width, self.__height)
