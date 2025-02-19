e2ec43c1a8051e2a989d28d5b9aeccbae4757bfa
Ashe2A <supersonicx001@gmail.com>
2025-02-19 15:41:21 +0100

    Abstract classes (0) - Animals

---
+++ python-abc/README.md
@@ -0,0 +1,1 @@
+# [Python - Abstract Classes and Interfaces](https://intranet.hbtn.io/projects/3104)
---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        raise TypeError("Can't instantiate abstract class Animal with abstract method sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+    
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
