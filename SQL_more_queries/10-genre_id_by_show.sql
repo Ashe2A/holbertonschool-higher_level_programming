76224f198f8eff63cc2a796b7ae026fd2a192c18
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:15:40 +0100

    SQL - More queries (10) - Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- genre id by show
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
