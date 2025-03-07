0268ee5425d3a8331b7b115e4c5b6fc9c744a2fb
Ashe2A <supersonicx001@gmail.com>
2025-03-07 11:26:51 +0100

    SQL - Introduction (9) - Multiple insert retry

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,11 +7,10 @@ CREATE TABLE IF NOT EXISTS second_table
 	score INT
 );
 
-INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10);
-INSERT INTO second_table (id, name, score)
-VALUES (1, "Alex", 3);
-INSERT INTO second_table (id, name, score)
-VALUES (1, "Bob", 14);
-INSERT INTO second_table (id, name, score)
-VALUES (1, "George", 8);
+INSERT INTO second_table
+(id, name, score)
+VALUES
+(1, "John", 10),
+(1, "Alex", 3),
+(1, "Bob", 14),
+(1, "George", 8);
