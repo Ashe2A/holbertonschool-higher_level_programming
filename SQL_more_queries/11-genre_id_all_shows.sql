a85dd520ce23ff80e5e4aa53fbb94b6da8f1ef9e
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:23:49 +0100

    SQL - More queries (10-11) - Useless join

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,5 +3,4 @@
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
