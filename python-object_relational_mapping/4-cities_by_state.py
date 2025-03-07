97340caf5e0f8334152a1b9280855517890eaf41
Ashe2A <supersonicx001@gmail.com>
2025-03-07 23:58:50 +0100

    Python - Object-relational mapping (4) - Fetch all cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Filter states by user input
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id;")
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    db.close()
