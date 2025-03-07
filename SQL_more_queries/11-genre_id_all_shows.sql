229e8bca9e9faad8779826fa5793a6ac025e604b
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:19:06 +0100

    SQL - More queries (11) - NULL display by left join

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- genre id all shows (even NULL ones)
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
