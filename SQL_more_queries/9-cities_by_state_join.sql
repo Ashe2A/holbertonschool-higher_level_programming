02db57015a89a0f9f7ed9450536b580c67ac8361
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:05:45 +0100

    SQL - More queries (9) - misplaced AS keyword

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- cities by state join query
 
-SELECT c.id AS id, c.name AS name, s.name AS name
+SELECT c.id AS id, c.name AS name, states.name AS name
 FROM cities AS c
-INNER JOIN states AS s ON c.state_id = states.id
+INNER JOIN states ON c.state_id = states.id
 ORDER BY c.id
