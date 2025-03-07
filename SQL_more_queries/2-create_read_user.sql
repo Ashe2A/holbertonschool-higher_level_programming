26debbe17633b8e34562c61bcd43bf367617e13a
Ashe2A <supersonicx001@gmail.com>
2025-03-07 19:52:27 +0100

    SQL - More queries (2) - database and reading user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- create database and user, and give them read perm (should not fail)
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost"
+IDENTIFIED BY "user_0d_2_pwd";
+GRANT READ PRIVILEGES ON *.* TO "user_0d_2"@"localhost";
