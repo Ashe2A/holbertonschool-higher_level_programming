a3d0abd159449ac35dcc5926563759d576962a84
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:36:52 +0100

    SQL - More queries (13) - name attribute in tv genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- no genre
 
-SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.id) AS number_of_shows
 FROM tv_show_genres
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY number_of_shows DESC;
