7cf5da9b63e3b5896a594ca0577434e44beaae27
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:21:47 +0100

    SQL - More queries (11) - Reverse : right join

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- genre id by show
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
