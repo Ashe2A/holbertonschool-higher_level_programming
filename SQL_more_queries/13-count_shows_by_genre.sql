b26f1a9f1a32a6d9383e3b41f01a187623e27097
Ashe2A <supersonicx001@gmail.com>
2025-03-07 21:35:58 +0100

    SQL - More queries (13) - Left join?

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 
 SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.id) AS number_of_shows
 FROM tv_show_genres
-RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY number_of_shows DESC;
