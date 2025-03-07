315bf0db021c8aa91a920b97e59224fc1013849a
Ashe2A <supersonicx001@gmail.com>
2025-03-07 22:46:00 +0100

    Python - Object-relational mapping (0) - refetch

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Get all states
+"""
 
 import MySQLdb
 from sys import argv
@@ -11,8 +14,8 @@ if __name__ == "__main__":
         db=argv[3]
     )
     c = server.cursor()
-    numrows = c.execute("SELECT * FROM states ORDER BY states.id;")
+    c.execute("SELECT * FROM states ORDER BY states.id;")
 
-    print(i for i in range(numrows))
+    print(i for i in c.fetchall())
     c.close()
     server.close()
