4b3b53733f30f659ee82383bcb44b50d1db6abd1
Ashe2A <supersonicx001@gmail.com>
2025-03-07 11:25:34 +0100

    SQL - Introduction (9) - Insert each line

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,8 +6,12 @@ CREATE TABLE IF NOT EXISTS second_table
 	name VARCHAR(256),
 	score INT
 );
+
 INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10),
-(1, "Alex", 3),
-(1, "Bob", 14),
-(1, "George", 8);
+VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "Alex", 3);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "Bob", 14);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "George", 8);
