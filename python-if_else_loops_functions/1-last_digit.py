a8831a1b087cb80703208f4f1bc6ec26dbe882b3
Ashe2A <supersonicx001@gmail.com>
2025-05-06 22:08:54 +0200

    Python - Hello, World (1) - condition for changing sign

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 moduloten = abs(number) % 10
-print("Last digit of {} is {:0.0f} and is ".
-      format(number, moduloten * number / abs(number)), end="")
-
-if moduloten > 5:
+if number < 0:
+    moduloten = -moduloten
+print("Last digit of {} is {} and is ".
+      format(number, moduloten), end="")
+if abs(moduloten) > 5:
     print("greater than 5")
-elif moduloten == 0:
+elif abs(moduloten) == 0:
     print("0")
 else:
     print("less than 6 and not 0")
