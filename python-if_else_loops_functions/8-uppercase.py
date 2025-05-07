0dddd7494bfdf19948ac2ba75e4dd3dc1d0f3a57
Ashe2A <supersonicx001@gmail.com>
2025-05-07 15:18:12 +0200

    Python - Hello, World (8) - rewrite characters and then print them

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     for i in str:
         if ord("a") <= ord(i) <= ord("z"):
-            print("{}".format(chr(ord(i) + ord("A") - ord("a"))), end="")
-        else:
-            print("{}".format(i), end="")
+            i = chr(ord(i) + ord("A") - ord("a"))
+        print("{}".format(i), end="")
     print()
