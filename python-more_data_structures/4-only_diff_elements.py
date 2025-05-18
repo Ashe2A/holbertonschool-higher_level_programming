b4d77afaddee142955f737ba4f4135b1c537a9fd
Ashe2A <supersonicx001@gmail.com>
2025-05-18 17:54:57 +0200

    Python - More Data Structures: Set, Dictionary (4) - Pycodestyle : E713 test for membership should be 'not in'

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -11,9 +11,9 @@ def only_diff_elements(set_1, set_2):
     """
     diff_set = []
     for i in set_1:
-        if not i in set_2:
+        if i not in set_2:
             diff_set.append(i)
     for i in set_2:
-        if not i in set_1:
+        if i not in set_1:
             diff_set.append(i)
     return set(diff_set)
