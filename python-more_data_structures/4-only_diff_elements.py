4a7b4fa5fbe45a7b0662ac902a347c70cdecdf57
Ashe2A <supersonicx001@gmail.com>
2025-05-18 17:53:59 +0200

    Python - More Data Structures: Set, Dictionary (4) - Forgot unique elements in the second set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -13,4 +13,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if not i in set_2:
             diff_set.append(i)
+    for i in set_2:
+        if not i in set_1:
+            diff_set.append(i)
     return set(diff_set)
