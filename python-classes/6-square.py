d3e06ddbd0e65829ec47088f8353b809ee8a4016
Ashe2A <supersonicx001@gmail.com>
2025-05-20 16:00:56 +0200

    Python - Classes and Objects (6) - private in setter not in init

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square():
     '''Square class'''
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         '''Area of the square
