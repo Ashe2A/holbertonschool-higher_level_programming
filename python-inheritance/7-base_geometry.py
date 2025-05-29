87f2d55cad4f34b7aeebe4e381cb03484ebf5323
Ashe2A <supersonicx001@gmail.com>
2025-05-30 01:11:44 +0200

    Python - Inheritance (7) - Integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Integer validator'''
+
+
+class BaseGeometry():
+    '''Base geometry class'''
+
+    def area(self):
+        '''Area of the base geometrical figure
+
+        Raises:
+            Exception: if area is not implemented in subclasses
+        '''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Integer validator for attributes
+
+        Args:
+            name (str): The name of the integer. Must always be string.
+            value (int): Value of the integer.
+
+        Raises:
+            TypeError: If value isn't an integer
+            ValueError: If value is 0 or under
+        """
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
