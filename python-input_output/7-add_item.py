8725cdbe8f2e56397640065f9fbef808dfd7cbc7
Ashe2A <supersonicx001@gmail.com>
2025-06-05 10:51:53 +0200

    Python - Input/Output (7) - pycodestyle: E402 module level import not at top of file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''Load, add, save'''
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 from sys import argv
 from os import path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 item_list = []
 if path.isfile('add_item.json'):
