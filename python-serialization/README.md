136c436118637e920efe2e0ae1e834bb29cddc99
Ashe2A <supersonicx001@gmail.com>
2025-06-06 23:43:21 +0200

    Python - Serialization (1) - Pickling Custom Classes

--- python-serialization/README.md
+++ python-serialization/README.md
@@ -3,5 +3,9 @@
 ## 0. Basic Serialization
 ### [`task_00_basic_serialization.py`](task_00_basic_serialization.py)
 * Functions that serialize a Python dictionary to a JSON file and deserialize the JSON file to recreate the Python Dictionary.
-### [`0-main.py`](0-main.py)
-* **Tests:** [`my_file_0.txt`](my_file_0.txt) text file encoded in UTF-8
+### [`main_00_basic_serialization.py`](main_00_basic_serialization.py)
+
+## 1. Pickling Custom Classes
+### [`task_01_pickle.py`](task_01_pickle.py)
+* `CustomObject` class which instances can be serialized and created upon deserialization using the `pickle` module.
+### [`main_01_pickle.py`](main_01_pickle.py)
---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Pickling Custom Classes"""
+import pickle
+
+
+class CustomObject():
+    """Custom class"""
+
+    def __init__(self, name, age, is_student):
+        """Custom constructor
+
+        Args:
+            name (str): Name of the object (?)
+            age (str): Age of the object (?)
+            is_student (bool): True if object is student, False otherwise
+        """
+        try:
+            self.name = name
+            self.age = age
+            self.is_student = is_student
+        except Exception as e:
+            print(e)
+
+    def display(self):
+        """Display info of the object"""
+        try:
+            print("Name: {}".format(self.name))
+            print("Age: {}".format(self.age))
+            print("Is Student: {}".format(self.is_student))
+        except Exception as e:
+            print(e)
+
+    def serialize(self, filename):
+        """Serialize the object into a pickle file
+
+        Args:
+            filename (str): The pickle file name
+        """
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception as e:
+            print(e)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize a pickle file to create an instance
+
+        Args:
+            filename (str): The pickle file name
+
+        Returns:
+            CustomObject: The new deserialized instance
+        """
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            return None
+        except Exception as e:
+            print(e)
+            return None
