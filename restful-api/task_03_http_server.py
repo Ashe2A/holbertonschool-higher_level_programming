1240012843bc4f0547a0bfd62975dd1f6a004377
Ashe2A <supersonicx001@gmail.com>
2025-06-13 16:11:17 +0200

    RESTful API (3) - Messages are made of plain text and sent responses are fine without weighing messages

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,42 +7,38 @@ import json
 class MyServer(BHRH):
     def do_GET(self):
         if self.path == "/":
-            self.send_response(200, "OK")
-            self.send_header("Content-type", "text/plain")
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
             self.send_response(200, "OK")
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(
                 {
                     "name": "John",
                     "age": 30,
                     "city": "New York"
-                }).encode())
+                }).encode("utf-8"))
         elif self.path == "/status":
-            self.send_response(200, "OK")
-            self.send_header("Content-type", "application/json")
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(json.dumps({
-                "message": "OK"
-                }).encode())
+            self.wfile.write(b"OK")
         elif self.path == "/info":
-            self.send_response(200, "OK")
-            self.send_header("Content-type", "application/json")
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps({
                 "version": "1.0",
                 "description": "A simple API built with http.server"
-                }).encode())
+                }).encode("utf-8"))
         else:
-            self.send_response(404, b"Endpoint not found")
-            self.send_header("Content-type", "application/json")
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.wfile.write(json.dumps({
-                "message": "Endpoint not found"
-                }).encode())
+            self.wfile.write(b"Endpoint not found")
 
 
 def run(server_class=HTTPServer, handler_class=MyServer):
