f9f6ab6439976e10b8cb6455101a9c2a2772777f
Ashe2A <supersonicx001@gmail.com>
2025-06-27 22:08:59 +0200

    SQL - More queries (10) - no need to join genres

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,4 @@ USE hbtn_0d_tvshows;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
