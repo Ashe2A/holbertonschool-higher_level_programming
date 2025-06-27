391725342abcd9c010eaf9120f69f64c173aa44d
Ashe2A <supersonicx001@gmail.com>
2025-06-27 23:43:29 +0200

    Python - Object-relational mapping (2) - Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        )
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE states.name = \"{}\" ORDER BY states.id;".format(argv[4]))
+    for i in c.fetchall():
+        print(i)
+
+    c.close()
+    db.close()
