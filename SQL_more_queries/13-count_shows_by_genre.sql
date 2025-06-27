361566338ce63dd084ff0504d5f6b76dc3eead64
Ashe2A <supersonicx001@gmail.com>
2025-06-27 22:42:51 +0200

    SQL - More queries (13) - Group by before order by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_show_genres
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY number_of_shows DESC
-GROUP BY tv_genres.name;
+GROUP BY genre
+ORDER BY number_of_shows DESC;
