322e7f94b46c2eba6cc3c9a15519dc31ecaa809f
Ashe2A <supersonicx001@gmail.com>
2025-06-27 20:57:11 +0200

    SQL - More queries (8) - bind ID to name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 
 SELECT cities.id AS id, cities.name AS name
 FROM cities, states
-WHERE states.name = "California"
+WHERE cities.state_id = state.id AND states.name = "California"
 ORDER BY cities.id;
