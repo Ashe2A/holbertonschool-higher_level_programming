fbfb023ca978bd2015bf48cfe6e3605738c01608
Ashe2A <supersonicx001@gmail.com>
2025-06-27 22:25:23 +0200

    SQL - More queries (12) - No genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- No genre
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
