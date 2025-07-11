aeb510c1fc7ee7a5c7f2d047b44703c125125c54
Ashe2A <supersonicx001@gmail.com>
2025-07-11 13:54:43 +0200

    JavaScript - Warm up (5) - isNaN

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const fi = parseInt(argv[2]);
-if (fi === 'NaN' || fi === undefined) {
+if (isNaN('NaN') || fi === undefined) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + fi);
