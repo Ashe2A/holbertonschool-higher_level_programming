74cfc8685244c0d05ca5147101b316ae246715b8
Ashe2A <supersonicx001@gmail.com>
2025-07-11 14:00:06 +0200

    JavaScript - Warm up (6) - for loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const { array } = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-array.forEach(element => {
-    console.log(element)
-});
+for (let i = 0; i < array.length; i++) {
+  console.log(i);
+}
