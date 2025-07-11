18dc27626f8c652103147d826054dd80f9134b07
Ashe2A <supersonicx001@gmail.com>
2025-07-11 13:55:15 +0200

    JavaScript - Warm up (5) - number as arg not nan

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const fi = parseInt(argv[2]);
-if (isNaN('NaN') || fi === undefined) {
+if (isNaN(fi) || fi === undefined) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + fi);
