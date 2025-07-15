b562f62cfd6bdd0fac3700f9976a15c716111603
Ashe2A <supersonicx001@gmail.com>
2025-07-15 12:35:42 +0200

    JavaScript DOM manipulation (1) - Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const redHeader = document.querySelector('#red_header');
+redHeader.addEventListener('click', () => {
+    header.style.color = '#FF0000';
+});
