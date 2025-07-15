ef7b1d57014220a5bb3480049148c2099bb20581
Ashe2A <supersonicx001@gmail.com>
2025-07-15 13:06:16 +0200

    JavaScript DOM manipulation (2) - Add `.red` class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,5 +2,5 @@
 const header = document.querySelector('header');
 const redHeader = document.querySelector('#red_header');
 redHeader.addEventListener('click', () => {
-  header.style.color = '#FF0000';
+  header.classList.add('red');
 });
