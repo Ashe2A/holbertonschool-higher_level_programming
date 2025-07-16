7e26b3881eeac11883c2494f6022b3904074273c
Ashe2A <supersonicx001@gmail.com>
2025-07-16 16:28:35 +0200

    JavaScript DOM manipulation (7) - Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+starWarsMovies();
+
+async function starWarsMovies () {
+  const urlFetch = await fetch('https://swapi-api.hbtn.io/api/films/?format=json');
+  const urlJson = await urlFetch.json();
+  const titles = document.querySelector('#list_movies');
+  const results = urlJson.results;
+  for (let i = 0; i < results.length; i++) {
+    const title = document.createElement('li');
+    title.appendChild(document.createTextNode(results[i].title));
+    titles.appendChild(title);
+  }
+}
