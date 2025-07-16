2f00785ed04818021f3fa22751bd80d56b73088f
Ashe2A <supersonicx001@gmail.com>
2025-07-16 13:58:58 +0200

    JavaScript DOM manipulation (3) - Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const toggleHeader = document.querySelector('#toggle_header');
+toggleHeader.addEventListener('click', () => {
+  if (header.classList.contains('green')) {
+    header.classList.add('red');
+    header.classList.remove('green');
+  } else {
+    header.classList.add('green');
+    header.classList.remove('red');
+  }
+});
