5ec050a993d25483ec37d56e672f3f013f58dc45
Ashe2A <supersonicx001@gmail.com>
2025-07-18 17:14:37 +0200

    Python - Server-Side Rendering (2) - Jinja if malformation and wrong args condition

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -5,17 +5,21 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 @app.route("/items")
 def contact():
@@ -25,6 +29,7 @@ def contact():
     except FileNotFoundError as e:
         print(str(e))
     return render_template("items.html", items=items)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
