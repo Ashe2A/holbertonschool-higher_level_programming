b776e2c8842e76d4e65ba28bb322278190e6a6e3
Ashe2A <supersonicx001@gmail.com>
2025-07-18 16:11:54 +0200

    Python - Server-Side Rendering (0) - "N/A" everywhere

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -17,10 +17,10 @@ def generate_invitations(template, attendees):
     output_no = 0
     for i in attendees:
         try:
-            name = str(i.get("name") or "N/A")
-            event_title = str(i.get("event_title") or "N/A")
-            event_date = str(i.get("event_date") or "N/A")
-            event_location = str(i.get("event_location") or "N/A")
+            name = str(i.get("name", "N/A") or "N/A")
+            event_title = str(i.get("event_title", "N/A") or "N/A")
+            event_date = str(i.get("event_date", "N/A") or "N/A")
+            event_location = str(i.get("event_location", "N/A") or "N/A")
 
             formatted = template.replace("{name}", name)
             formatted = formatted.replace("{event_title}", event_title)
