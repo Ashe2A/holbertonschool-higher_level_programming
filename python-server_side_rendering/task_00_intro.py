946deb1f6a28f299392fd672709a9bafb9d7ca07
Ashe2A <supersonicx001@gmail.com>
2025-07-18 15:54:24 +0200

    Python - Server-Side Rendering (0) - new_template

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from data_00_intro import attendees
-
 def generate_invitations(template, attendees):
     if not isinstance(template, str):
         raise TypeError(str("Template should be a string"))
@@ -14,33 +12,29 @@ def generate_invitations(template, attendees):
         raise ValueError(str("The list of attendees should not be empty"))
 
     output_no = -1
-    for i in attendees:
-        if "name" in i:
-            name = i["name"]
-        else:
+    for i in attendees:            
+        name = i["name"]
+        if name is None:
             name = "N/A"
-
-        if "event_title" in i:
-            event_title = i["event_title"]
-        else:
+            
+        event_title = i["event_title"]
+        if event_title is None:
             event_title = "N/A"
 
-        if "event_date" in i:
-            event_date = i["event_date"]
-        else:
+        event_date = i["event_date"]
+        if event_date is None:
             event_date = "N/A"
 
-        if "event_location" in i:
-            event_location = i["event_location"]
-        else:
+        event_location = i["event_location"]
+        if event_location is None:
             event_location = "N/A"
-        
-        template = template.replace("{name}", name)
-        template = template.replace("{event_title}", event_title)
-        template = template.replace("{event_date}", event_date)
-        template = template.replace("{event_location}", event_location)
+
+        new_template = template.replace("{name}", name)
+        new_template = new_template.replace("{event_title}", event_title)
+        new_template = new_template.replace("{event_date}", event_date)
+        new_template = new_template.replace("{event_location}", event_location)
         
         output_no += 1
 
         with open("output_" + str(output_no) + ".txt", "w", encoding="utf-8") as f:
-            return f.write(template)
+            f.write(new_template)
