b3c3b02b5fc2804c4c5d32734b840367e26861b1
Ashe2A <supersonicx001@gmail.com>
2025-07-23 09:25:53 +0200

    Python - Everything is object (3) - test documentation

--- python-everything_is_object/3-test.py
+++ python-everything_is_object/3-test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Tests for Right count ="""
 a = 89
 b = 89
 
 with open("3-answer.txt", "w") as f:
+    """If a and b point to the same adress,
+    write Yes in 3-answer.txt, otherwise No"""
     if id(a) == id(b):
         f.write("Yes\n")
     else:
