c99f4554c8d455be0bee387c3ca7b71c2ebf750a
Ashe2A <supersonicx001@gmail.com>
2025-07-23 12:31:20 +0200

    Python - Everything is object (29) - take a picture

--- python-everything_is_object/28-test.py
+++ python-everything_is_object/28-test.py
@@ -2,13 +2,8 @@
 """Tests for Same or not?"""
 a = [1, 2, 3]
 id_a = id(a)
+print("a id: {}".format(id_a))
 a += [4]
 new_id_a = id(a)
-
-with open("28-answer.txt", "w") as f:
-    """Write whether a keeps the same address
-    upon being incremented a value in 28-answer.txt"""
-    if id_a == new_id_a:
-        f.write("Yes\n")
-    else:
-        f.write("No\n")
+print("a new id: {}".format(new_id_a))
+print("Are both id the same?: {}".format(id_a == new_id_a))
