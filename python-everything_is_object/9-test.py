4cca688864dc1a914e34d99f9188eaf136d606c6
Ashe2A <supersonicx001@gmail.com>
2025-07-23 09:28:49 +0200

    Python - Everything is object (9) - no newline for print answers

--- python-everything_is_object/9-test.py
+++ python-everything_is_object/9-test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 s1 = "Best School"
 s2 = "Best School"
-print(s1 is s2)
+
+with open("9-answer.txt", "w") as f:
+    """If s1 and s2 are the same object,
+    write True in 9-answer.txt, otherwise False"""
+    f.write("{}".format(s1 is s2))
