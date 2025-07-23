448c193005ed22ebcbba57689141ca6d269219a0
Ashe2A <supersonicx001@gmail.com>
2025-07-23 10:39:18 +0200

    Python - Everything is object (27) - answer format fix

--- python-everything_is_object/27-test.py
+++ python-everything_is_object/27-test.py
@@ -8,4 +8,7 @@ new_id_a = id(a)
 with open("27-answer.txt", "w") as f:
     """Write whether a keeps the same address
     upon being added a value in 27-answer.txt"""
-    f.write("{}\n".format(id_a == new_id_a))
+    if id_a == new_id_a:
+        f.write("Yes\n")
+    else:
+        f.write("No\n")
