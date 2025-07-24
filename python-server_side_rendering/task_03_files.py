006b19946f8f1d78a3ccea11ea31260eb676dd5c
Ashe2A <supersonicx001@gmail.com>
2025-07-24 17:24:11 +0200

    Python - Server-Side Rendering (3) - Displaying Data from JSON or CSV Files in Flask

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, request
+import json
+import csv
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/items")
+def items():
+    try:
+        with open("items.json", "r") as f:
+            data = json.load(f)
+            items = data["items"]
+    except Exception as e:
+        print(str(e))
+        items = []
+
+    return render_template("items.html", items=items)
+
+
+@app.route("/product_display")
+def product_display():
+    source = request.args["source"]
+    id = request.args["id"]
+
+    try:
+        if source == "json":
+            with open("products.json", "r") as f:
+                products = json.load(f)
+        elif source == "csv":
+            with open("products.csv") as f:
+                products = list(csv.DictReader(f))
+        else:
+            return render_template("product_display.html",
+                                   error="Wrong source")
+
+        if id:
+            if not id:
+                product_display = products
+            else:
+                product_display = []
+                for i in products:
+                    if id and str(i["id"]) == id:
+                        product_display.append(i)
+                    else:
+                        return render_template("product_display.html",
+                                               message="Product not found.")
+            return render_template("product_display.html",
+                                   products=product_display)
+    except Exception as e:
+        return render_template("product_display.html", error=e)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
